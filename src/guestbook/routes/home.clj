0d8b670945132f004ec22ba550ccabfab8d264b9
Willa <mowmowjoker@gmail.com>
2014-03-03 12:53:38 -0500

    Added show-guests function and save message function to render in UI.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,9 +1,47 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home [] 
-  (layout/common [:h1 "Hello World!"]))
+
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Hoody" :name "Willa" :timestamp nil}
+          {:message "Heelo" :name "Will" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]]
+     )])
+
+
+(defn home [& [name message error]]
+  (layout/common
+     [:h1 "Guest Book!"]
+     [:p "Welcome to my guestbook"]
+     [:p error]
+     (show-guests)
+     [:hr]
+     (form-to [:post "/"]
+      [:p "Name:"]
+      (text-field "name" name)
+      [:p "Message:"]
+      (text-area {:rows 10 :cols 40} "message" message)
+      [:br]
+      (submit-button "comment"))))
+
+(defn save-message [name message]
+  (cond
+   (empty? name)
+   (home name message "No name?")
+   (empty? message)
+   (home name message "No Message?")
+   :else
+   (do
+     (println name message)
+     (home))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message)))
